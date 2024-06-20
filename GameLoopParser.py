d09a8c164da67fb12984797a23f6a7ae63aaf9a5
plagunas2 <priscilagunas@gmail.com>
2024-06-20 13:25:50 +0200

    began python game loop parser

--- GameLoopParser.py
+++ GameLoopParser.py
@@ -1,2 +1,22 @@
 #parses for main game loops to translate to browser-compatible loops
-#using JS's requestAnimationFrame or emscipten's func
+#using JS's requestAnimationFrame, emscipten's func, or pygbag's async
+
+
+''' python / pygbag parser 
+import asyncio and change game loop to async def main()
+move global vars to before loop
+in main loop, after pygame.display.update(), include await asyncio.sleep(0)
+end script with asyncio.run(main())
+
+'''
+
+import pygbag
+import os
+import ast
+
+global_vars = []
+
+def parsePython(main_loop_path):
+    with open(main_loop_path, 'a') as ml:
+        ml.write("PLACEHOLDER")
+
